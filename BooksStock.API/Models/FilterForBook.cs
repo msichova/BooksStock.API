7590e2fbe23b115a89be255a410e158cc7baa23c
Marina Sichova <msichova@outlook.com>
2024-02-17 17:01:52 +0800

    Create Models\FilterForBook.cs
    Model for filtering Books by entered parameters

---
+++ BooksStock.API/Models/FilterForBook.cs
@@ -0,0 +1,25 @@
+﻿/*
+ * 
+ * Model for filtering Books by entered parameters
+ */
+using System.ComponentModel.DataAnnotations;
+
+namespace BooksStock.API.Models
+{
+    public class FilterForBook
+    {
+        public string? Title {  get; set; }
+        public string? Author {  get; set; }
+        public string? Annotation { get; set; }
+        public string? Language { get; set; }
+        public string[]? Genres { get; set; }
+        public bool? IsAvailable { get; set; }
+
+        [Range(0, int.MaxValue)]
+        public decimal? MinPrice { get; set;}
+
+        [Range(0, int.MaxValue)]
+        public decimal? MaxPrice { get; set; }
+        
+    }
+}
