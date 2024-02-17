6830df4f37af4e0f0b389f0a0ae4e0584945cda1
Marina Sichova <msichova@outlook.com>
2024-02-17 14:49:20 +0800

    Update Program.cs
    Registering service to the DI container StockServices.
    Registering service to the DI container StockDatabaseSettings and adds configuration source to it.

--- BooksStock.API/Program.cs
+++ BooksStock.API/Program.cs
@@ -1,6 +1,11 @@
+using BooksStock.API.Models;
+using BooksStock.API.Services;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.Configure<StockDatabseSettings>(builder.Configuration.GetSection("ConnectionToMongoDB"));
+builder.Services.AddSingleton<StockServices>();
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
