ecb97e74615e11dcf85d7379a242a543cf4507fd
Marina Sichova <msichova@outlook.com>
2024-02-17 15:59:22 +0800

    Create Models\PageModel.cs
     Page Model from user request

---
+++ BooksStock.API/Models/PageModel.cs
@@ -0,0 +1,12 @@
+﻿/*
+ * Page Model from user request
+ */
+namespace BooksStock.API.Models
+{
+    public class PageModel
+    {
+        public int CurrentPage { get; set; } = Query.MinPage;
+        public int QuantityPerPage { get; set; } = Query.MaxPerPage;
+        public bool? InAscendingOrder { get; set; } = true;
+    }
+}
