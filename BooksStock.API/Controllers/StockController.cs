584ce5963c221017573c2f97a1ecf943d016bb16
Marina Sichova <msichova@outlook.com>
2024-02-17 14:58:36 +0800

    Create Controllers\StockController.cs
    Added Controller with class StockV1Controller(StockServices, ILogger<StockV1Controller>): ControllerBase, with one HttpGet method:
    async Task<ActionResult<List<BookProduct>>> GetAllBooks(), that returns the whole collection. !ATTENTION! may be overflow error, or slowdown performance. Depends on current size of database. Returns all data from database collection

---
+++ BooksStock.API/Controllers/StockController.cs
@@ -0,0 +1,50 @@
+﻿using BooksStock.API.Models;
+using BooksStock.API.Services;
+using Microsoft.AspNetCore.Mvc;
+using System.Drawing;
+
+namespace BooksStock.API.Controllers
+{
+    [ApiController]
+    [Produces("application/json")]
+    [Consumes("application/json")]
+    public class StockV1Controller(StockServices services, ILogger<StockV1Controller> logger) : ControllerBase
+    {
+        private readonly StockServices _services = services;
+        private readonly ILogger<StockV1Controller> _logger = logger;
+
+        #region of HttpGet methods
+
+        #region simple HttpGet methods
+        /*!ATTENTION! may be overflow error, or slowdown preformance.Depends on current size of database
+         * returns all data from database collection
+        */
+        [HttpGet, Route("all-books")]
+        public async Task<ActionResult<List<BookProduct>>> GetAllBooks()
+        {
+            try
+            {
+                var books = await _services.GetAllBooksAsync();
+                return books is null || books.Count == 0 ? 
+                    NotFound("There no data found in Collection to display") : Ok(books);
+            }
+            catch (Exception error)
+            {
+                MyLogErrors(error);
+                return Problem(error.Message.ToString());
+            }
+        }
+        #endregion
+
+
+        #endregion
+
+        #region of Helpper methods
+        private void MyLogErrors(Exception error)
+        {
+            _logger.LogError(message: error.Message, args: error.StackTrace);
+        }
+
+        #endregion
+    }
+}
