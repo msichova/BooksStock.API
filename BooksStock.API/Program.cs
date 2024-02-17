6bc85b75509454771d4a4d8bf281ecd9d0d3905a
Marina Sichova <msichova@outlook.com>
2024-02-17 15:14:23 +0800

    Update Program.cs
    Added support to logging with Serilog and Added middleware for streamlined request logging

--- BooksStock.API/Program.cs
+++ BooksStock.API/Program.cs
@@ -1,5 +1,6 @@
 using BooksStock.API.Models;
 using BooksStock.API.Services;
+using Serilog;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -12,6 +13,9 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+//Add support to logging with SERILOG
+builder.Host.UseSerilog((context, configuration) => configuration.ReadFrom.Configuration(context.Configuration));
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -20,6 +24,8 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
+
+app.UseSerilogRequestLogging();
 
 app.UseHttpsRedirection();
 
