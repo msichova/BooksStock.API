d4903e5ad24ad16e2ea23ae6f4bdc4225e9e4e09
Marina Sichova <msichova@outlook.com>
2024-02-17 13:17:12 +0800

    Create Models/StockDatabseSettings.cs
    Added class to store the appsettings.json file's ConnectionToMongoDB property values

---
+++ BooksStock.API/Models/StockDatabseSettings.cs
@@ -0,0 +1,13 @@
+﻿/*
+ * Class to store the appsettings.json file's ConnectionToMongoDB property values
+ * 
+ */
+namespace BooksStock.API.Models
+{
+    public class StockDatabseSettings
+    {
+        public string Connection { get; set; } = null!;
+        public string DatabaseName { get; set; } = null!;
+        public string CollectionName { get; set; } = null!;
+    }
+}
