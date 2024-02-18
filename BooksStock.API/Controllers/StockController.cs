1c3b3fe230f94c74c3c0f4652d734f6de48c3bf2
Marina Sichova <msichova@outlook.com>
2024-02-18 12:22:26 +0800

    Update Controllers\StockController.cs
    Added API versioning, API Versioning attributes, and new class StockV2Controller(StockServices, ILogger<StockV2Controller>) : ControllersBase for API Verstion 2.
    This version only for Users, retrives some data from Collection, where data.IsAvailable == true. Supports only some Http Get methods

--- BooksStock.API/Controllers/StockController.cs
+++ BooksStock.API/Controllers/StockController.cs
@@ -1,4 +1,5 @@
-﻿using BooksStock.API.Models;
+﻿using Asp.Versioning;
+using BooksStock.API.Models;
 using BooksStock.API.Services;
 using Microsoft.AspNetCore.Mvc;
 using MongoDB.Bson;
@@ -8,6 +9,11 @@ using System.Drawing;
 
 namespace BooksStock.API.Controllers
 {
+    /*
+     * Version for Admin to retrive all data and manipulates with data
+     * Supports all methods: HttpGet, HttpPost, HttpPut and HttpDelete
+     */
+    [ApiVersion("1.0")]
     [ApiController]
     [Produces("application/json")]
     [Consumes("application/json")]
@@ -515,6 +521,357 @@ namespace BooksStock.API.Controllers
 
                     booksMatches = filter.IsAvailable.HasValue ?
                         booksMatches.Where(book => book.IsAvailable == filter.IsAvailable).ToList() :
+                        booksMatches;
+
+                    booksMatches = filter.MinPrice.HasValue && filter.MaxPrice.HasValue ?
+                        booksMatches.Where(book => book.Price >= filter.MinPrice && book.Price <= filter.MaxPrice).ToList() :
+                        filter.MinPrice.HasValue && !filter.MaxPrice.HasValue ?
+                        booksMatches.Where(book => book.Price >= filter.MinPrice).ToList() :
+                        !filter.MinPrice.HasValue && filter.MaxPrice.HasValue ?
+                        booksMatches.Where(book => book.Price <= filter.MaxPrice).ToList() :
+                        booksMatches;
+
+                    return booksMatches;
+                }
+                return [];
+            }
+            catch (Exception error)
+            {
+                MyLogErrors(error);
+                return [];
+            }
+        }
+        #endregion
+    }
+    /*
+     * Version for Users, retrives some data from Collection, where data.IsAvailable == true
+     * Supports only some Http Get methods
+    */
+    [ApiVersion("2.0")]
+    [ApiController]
+    [Consumes("application/json")]
+    [Produces("application/json")]
+    public class StockV2Controller(StockServices services, ILogger<StockV2Controller> logger) : ControllerBase
+    {
+        private readonly StockServices _services = services;
+        private readonly ILogger<StockV2Controller> _logger = logger;
+
+        #region of HttpGet methods
+
+        #region simple HttpGet methods
+        /*!ATTENTION! may be overflow error, or slowdown preformance.Depends on current size of database
+         * returns all data from database collection, where parameter IsAvailable == true
+        */
+        [HttpGet, Route("all-books")]
+        public async Task<ActionResult<List<BookProduct>>> GetAllBooks()
+        {
+            try
+            {
+                var books = (await _services.GetAllBooksAsync()).Where(book => book.IsAvailable).ToList();
+                return books is null || books.Count == 0 ?
+                    NotFound("There no data found in Collection to display") : Ok(books);
+            }
+            catch (Exception error)
+            {
+                MyLogErrors(error);
+                return Problem(error.Message.ToString());
+            }
+        }
+
+
+        [HttpGet, Route("books-on-page")]
+        public async Task<ActionResult<BookProduct>> GetPerPage([FromQuery] PageModel pageModel)
+        {
+            try
+            {
+                var books = (await _services.GetAllBooksAsync()).Where(book => book.IsAvailable).ToList();
+                if (books is null || books.Count == 0)
+                {
+                    return NotFound("There no data found in Collection to display");
+                }
+                List<BookProduct> booksOnPage = ContentOnPage(books, pageModel);
+
+                return booksOnPage is null || booksOnPage.Count == 0 ?
+                    NotFound("There no data found for this page: " + pageModel.CurrentPage) :
+                    Ok(booksOnPage);
+            }
+            catch (Exception error)
+            {
+                MyLogErrors(error);
+                return Problem(error.Message.ToString());
+            }
+        }
+
+        //returns list of all genres in collection, where at least one book IsAvailable == true
+        [HttpGet, Route("genres")]
+        public async Task<ActionResult<string>> GetAllGenres()
+        {
+            try
+            {
+                var books = (await _services.GetAllBooksAsync()).Where(book => book.IsAvailable).ToList();
+
+                if (books is not null)
+                {
+                    List<string> genres = [];
+                    foreach (var book in books)
+                    {
+                        if (book.Genres!.Length != 0)
+                        {
+                            genres.AddRange(book.Genres.ToArray().Where(genre => !genres.Contains(genre)).ToList());
+                        }
+                    }
+                    return genres is null || genres.Count == 0 ?
+                        NotFound("There no data in Collection") : Ok(genres);
+                }
+                return NotFound("There no data in Collection");
+            }
+            catch (Exception error)
+            {
+                MyLogErrors(error);
+                return Problem(error.Message.ToString());
+            }
+        }
+        #endregion
+
+        #region of Filtering methods
+        //returns list of ALL books.IsAvailable where Title,Author, Language or one of item from array of Genres EQUALS to searchCondition
+        [HttpGet, Route("books-equals-condition")]
+        public async Task<ActionResult<List<BookProduct>>> GetAllBookEquals([Required] string term)
+        {
+            try
+            {
+                var books = (await _services.GetAllBooksEqualsConditionAsync(term)).Where(book => book.IsAvailable).ToList();
+                return books is null || books.Count == 0 ?
+                    NotFound("There no data in Collection that equals to: '" + term + "'") : Ok(books);
+            }
+            catch (Exception error)
+            {
+                MyLogErrors(error);
+                return Problem(error.Message.ToString());
+            }
+        }
+
+        //returns list of books.IsAvailable AT the REQUESTED Page where Title,Author, Language or one of item from array of Genres EQUALS to search term
+        [HttpGet, Route("books-equals-coondition-page")]
+        public async Task<ActionResult<List<BookProduct>>> GetPerPageBookEquals([Required] string term, [FromQuery] PageModel pageModel)
+        {
+            try
+            {
+                var books = (await _services.GetAllBooksEqualsConditionAsync(term)).Where(book => book.IsAvailable).ToList();
+                if (books is null || books.Count == 0)
+                {
+                    return NotFound("There no data in Collection that equals to: '" + term + "'");
+                }
+
+                List<BookProduct> booksOnReturn = ContentOnPage(books, pageModel);
+
+                return booksOnReturn is null || booksOnReturn.Count == 0 ?
+                    NotFound("There no data in Collection that equals to: '" + term + "', on requested page = '" + pageModel.CurrentPage + "'.") :
+                    Ok(booksOnReturn);
+            }
+            catch (Exception error)
+            {
+                MyLogErrors(error);
+                return Problem(error.Message.ToString());
+            }
+        }
+
+        //returns list of ALL books.IsAvailable where Title,Author, Language or one of item from array of Genres CONTAINS to searchCondition
+        [HttpGet, Route("books-contains-condition")]
+        public async Task<ActionResult<List<BookProduct>>> GetAllBookContains([Required] string term)
+        {
+            try
+            {
+                var books = (await _services.GetAllBooksContainsConditionAsync(term)).Where(book => book.IsAvailable).ToList();
+                return books is null || books.Count == 0 ?
+                    NotFound("There no data in Collection that contains: '" + term + "'") : Ok(books);
+            }
+            catch (Exception error)
+            {
+                MyLogErrors(error);
+                return Problem(error.Message.ToString());
+            }
+        }
+
+        //returns list of books.IsAvailable AT the REQUESTED Page where Title,Author, Language or one of item from array of Genres CONTAINS to search term
+        [HttpGet, Route("books-contains-coondition-page")]
+        public async Task<ActionResult<List<BookProduct>>> GetPerPageBookContains([Required] string term, [FromQuery] PageModel pageModel)
+        {
+            try
+            {
+                var books = (await _services.GetAllBooksContainsConditionAsync(term)).Where(book => book.IsAvailable).ToList();
+                if (books is null || books.Count == 0)
+                {
+                    return NotFound("There no data in Collection that contains: '" + term + "'");
+                }
+
+                List<BookProduct> booksOnReturn = ContentOnPage(books, pageModel);
+
+                return booksOnReturn is null || booksOnReturn.Count == 0 ?
+                    NotFound("There no data in Collection that contains: '" + term + "', on requested page = '" + pageModel.CurrentPage + "'.") :
+                    Ok(booksOnReturn);
+            }
+            catch (Exception error)
+            {
+                MyLogErrors(error);
+                return Problem(error.Message.ToString());
+            }
+        }
+
+        /* !ATTENTION! may be overflow error, or slowdown preformance. Depends on current size of database
+         * matching books.IsAvailable in collection by entered filters, where athour equals author, title equals title and etc.
+         * if there was not entered any searching parameters returns all list of collection 
+         */
+        [HttpGet, Route("books-filtered")]
+        public async Task<ActionResult<List<BookProduct>>> GetAllUnderFilter([FromQuery] FilterForBook filter)
+        {
+            try
+            {
+                var books = (await _services.GetAllBooksAsync()).Where(book => book.IsAvailable).ToList();
+
+                if (books is null || books.Count == 0)
+                {
+                    return NotFound("There no data in Collection");
+                }
+                List<BookProduct> booksMatches = ApplyFilters(books, filter);
+
+                return booksMatches is null || booksMatches.Count == 0 ?
+                    NotFound("There no data in Collection by requested filter: \n" + filter.ToJson()) :
+                    Ok(booksMatches);
+            }
+            catch (Exception error)
+            {
+                MyLogErrors(error);
+                return Problem(error.Message.ToString());
+            }
+        }
+
+        /* !ATTENTION! may be overflow error, or slowdown preformance. Depends on current size of database
+         * matching books.IsAvailable, AT the REQUESTED Page, in collection by entered filters, where athour equals author, 
+         * title equalstitle and etc.
+         * if there was not entered any searching parameters returns all list of collection 
+        */
+        [HttpGet, Route("books-filtered-page")]
+        public async Task<ActionResult<List<BookProduct>>> GetPerPageUnderFilter([FromQuery] FilterForBook filter, [FromQuery] PageModel pageModel)
+        {
+            try
+            {
+                var books = (await _services.GetAllBooksAsync()).Where(book => book.IsAvailable).ToList();
+
+                if (books is null || books.Count == 0)
+                {
+                    return NotFound("There no data in Collection");
+                }
+                List<BookProduct> booksMatches = ApplyFilters(books, filter);
+                if (booksMatches is null || booksMatches.Count == 0)
+                {
+                    NotFound("There no data in Collection by requested filter: \n" + filter.ToJson());
+                }
+                booksMatches = ContentOnPage(booksMatches!, pageModel);
+
+                return booksMatches is null || booksMatches.Count == 0 ?
+                    NotFound("There no data in Collection that contains:\n'" + filter.ToJson() + "',\non requested page = '" + pageModel.CurrentPage + "'.") :
+                    Ok(booksMatches);
+            }
+            catch (Exception error)
+            {
+                MyLogErrors(error);
+                return Problem(error.Message.ToString());
+            }
+        } 
+        #endregion
+
+        #region of Counting Methods
+        //returning a number of all books.IsAvailable in collection
+        [HttpGet, Route("all-books-count")]
+        public async Task<ActionResult<int>> GetQuantityOfAllBooks()
+        {
+            try
+            {
+                int quantity = (await _services.GetAllBooksAsync()).Where(book => book.IsAvailable).ToList().Count;
+
+                return Ok(quantity);
+            }
+            catch (Exception error)
+            {
+                MyLogErrors(error);
+                return Problem(error.Message.ToString());
+            }
+        }
+
+        //returning a number of all books.IsAvailable under particular genre
+        [HttpGet, Route("books-genre-count")]
+        public async Task<ActionResult<int>> GetQuantityAtGenre([Required] string genre)
+        {
+            try
+            {
+                return (await _services.GetAllBooksAsync()).Any(book => book.Genres!.Contains(genre) && book.IsAvailable) ?
+                    Ok((await _services.GetAllBooksAsync()).Where(book => book.Genres!.Contains(genre) && book.IsAvailable).ToList().Count) :
+                    NotFound("The genre = '" + genre + "', was not found in Collection");
+            }
+            catch (Exception error)
+            {
+                MyLogErrors(error);
+                return Problem(error.Message.ToString());
+            }
+        }
+        #endregion
+        #endregion
+        #region of Helpper methods
+        private void MyLogErrors(Exception error)
+        {
+            _logger.LogError(message: error.Message, args: error.StackTrace);
+        }
+
+        //returning content on particular page divided by particular quantity per page
+        //sorting returning List by default(if not specified) in ascending order by price
+        private List<BookProduct> ContentOnPage(List<BookProduct> allBooks, PageModel pageModel)
+        {
+            try
+            {
+                List<BookProduct> booksOnReturn = !pageModel.InAscendingOrder.HasValue ||
+                    pageModel.InAscendingOrder == true ?
+                    [.. allBooks.OrderBy(book => book.Price)] :
+                    [.. allBooks.OrderByDescending(book => book.Price)];
+
+                Query query = new(pageModel.CurrentPage, (pageModel.QuantityPerPage < Query.MinPerPage ? Query.MinPerPage : pageModel.QuantityPerPage), booksOnReturn.Count);
+
+                return booksOnReturn is null || booksOnReturn.Count == 0 ?
+                    [] : [.. booksOnReturn.Skip(query.ToSkipQuantity).Take(query.QuantityPerPage)];
+            }
+            catch (Exception error)
+            {
+                MyLogErrors(error);
+                return [];
+            }
+        }
+
+        /*
+         * Method returning list of books where items contains all FilterForBook criterias, 
+         * expect the 'IsAvailable' parameter not applys, because 2 API Version only has access where IsAvailable == true only
+        */
+        private List<BookProduct> ApplyFilters(List<BookProduct> allBooks, FilterForBook filter)
+        {
+            try
+            {
+                if (allBooks is not null || allBooks!.Count != 0)
+                {
+                    List<BookProduct> booksMatches = [.. allBooks];
+
+                    booksMatches = !string.IsNullOrEmpty(filter.Author) ?
+                        booksMatches.Where(book => book.Author!.Contains(filter.Author, StringComparison.OrdinalIgnoreCase)).ToList() : booksMatches;
+
+                    booksMatches = !string.IsNullOrEmpty(filter.Title) ?
+                        booksMatches.Where(book => book.Title!.Contains(filter.Title, StringComparison.OrdinalIgnoreCase)).ToList() : booksMatches;
+
+                    booksMatches = !string.IsNullOrEmpty(filter.Annotation) ?
+                        booksMatches.Where(book => book.Description!.Contains(filter.Annotation, StringComparison.OrdinalIgnoreCase)).ToList() : booksMatches;
+
+                    booksMatches = !string.IsNullOrEmpty(filter.Language) ?
+                        booksMatches.Where(book => book.Language!.Contains(filter.Language, StringComparison.OrdinalIgnoreCase)).ToList() : booksMatches;
+
+                    booksMatches = filter.Genres is not null ?
+                        booksMatches.Where(book => book.Genres!.Any(genre => filter.Genres.Any(g => g.Contains(genre, StringComparison.OrdinalIgnoreCase)))).ToList() :
                         booksMatches;
 
                     booksMatches = filter.MinPrice.HasValue && filter.MaxPrice.HasValue ?
