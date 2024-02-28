5272f825edbca02d909d44a5af4ca61e71e1d39b
Marina Sichova <msichova@outlook.com>
2024-02-28 12:31:26 +0800

    Create BooksStock.API\Services\ApiKey\IApiKeyValidator.cs
    Added interface to validate API key

---
+++ BooksStock.API/Services/ApiKey/IApiKeyValidator.cs
@@ -0,0 +1,11 @@
+﻿/*
+ * Interface to validate API key
+ * 
+ */
+namespace BooksStock.API.Services.ApiKey
+{
+    public interface IApiKeyValidator
+    {
+        bool IsValid(string apiKey);
+    }
+}
