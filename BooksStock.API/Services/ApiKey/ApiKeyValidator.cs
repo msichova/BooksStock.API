f164c0fc4222a8105e8d38b0c778c3ddc10b8e3b
Marina Sichova <msichova@outlook.com>
2024-02-28 12:46:38 +0800

    Create BooksStock.API\Services\ApiKey\ApiKeyValidator.cs
    Added class ApiKeyValidator(IConfiguration) that implements IApiKeyValidator interface, class IsValid(string) and injecting the IConfiguration into the consturctor

---
+++ BooksStock.API/Services/ApiKey/ApiKeyValidator.cs
@@ -0,0 +1,21 @@
+﻿/**
+ * Implements the IApiKeyValidator interface class IsValid(string)
+ * and injecting the IConfiguration into the constructor
+ * 
+ **/
+namespace BooksStock.API.Services.ApiKey
+{
+    public class ApiKeyValidator(IConfiguration configuration) : IApiKeyValidator
+    {
+        private readonly IConfiguration _configuration = configuration;
+
+        public bool IsValid(string apiKey)
+        {
+            if(string.IsNullOrEmpty(apiKey)) { return false; }
+
+            string? key = _configuration.GetValue<string>(ApiConstants.ApiKeyName);
+
+            return !string.IsNullOrEmpty(key) && key.Equals(apiKey);
+        }
+    }
+}
