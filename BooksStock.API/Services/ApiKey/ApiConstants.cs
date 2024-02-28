cec640d30f2ed52c9b49b2b32fcf7ad1b8757490
Marina Sichova <msichova@outlook.com>
2024-02-28 12:37:39 +0800

    Create BooksStock.API\Services\ApiKey\ApiConstants.cs

---
+++ BooksStock.API/Services/ApiKey/ApiConstants.cs
@@ -0,0 +1,12 @@
+﻿/*
+ * API constants
+ * 
+ */
+namespace BooksStock.API.Services.ApiKey
+{
+    public static class ApiConstants
+    {
+        public const string ApiKeyHeader = "x-API-key";
+        public const string ApiKeyName = "ApiKey";
+    }
+}
