b8decd0086301121e0137db5a163bff8e24d7cb7
Marina Sichova <msichova@outlook.com>
2024-02-29 11:23:10 +0800

    Create LoginModel.cs

---
+++ BooksStock.API/Models/Authentication/LoginModel.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace BooksStock.API.Models.Authentication
+{
+    public class LoginModel
+    {
+        [Required(ErrorMessage = "User login required")]
+        public string UserLogin {  get; set; } = string.Empty!;
+
+        [Required(ErrorMessage = "Password is required")]
+        [PasswordPropertyText(true)]
+        [DataType(DataType.Password)] 
+        public string Password { get; set; } = string.Empty!;
+    }
+}
