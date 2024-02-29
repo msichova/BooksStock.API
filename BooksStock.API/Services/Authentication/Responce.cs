9b136b8a1f94cc8ade31ed43d38b777812af39bc
Marina Sichova <msichova@outlook.com>
2024-02-29 11:30:29 +0800

    Create Responce.cs
    Class to hold response values after login and registration

---
+++ BooksStock.API/Services/Authentication/Responce.cs
@@ -0,0 +1,12 @@
+﻿/**
+ * To hold response values after login and registration
+ * 
+ **/
+namespace BooksStock.API.Services.Authentication
+{
+    public class Responce
+    {
+        public string ? Status { get; set; }
+        public string ? Message { get; set; }
+    }
+}
