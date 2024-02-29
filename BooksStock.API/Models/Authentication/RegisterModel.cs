9d45576a6f91b4bede38b654c9d4d7639b618a7b
Marina Sichova <msichova@outlook.com>
2024-02-29 11:28:35 +0800

    Create RegisterModel.cs

---
+++ BooksStock.API/Models/Authentication/RegisterModel.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace BooksStock.API.Models.Authentication
+{
+    public class RegisterModel
+    {
+        [Required(ErrorMessage = "Unique User Login is Required")]
+        public string Login { get; set; } = null!;
+
+        [Required(ErrorMessage = "Unique Email is Required")]
+        [EmailAddress]
+        [DataType(DataType.EmailAddress)]
+        public string Email { get; set; } = null!;
+
+        [Required(ErrorMessage = "Password is Required")]
+        [DataType(DataType.Password)]
+        [PasswordPropertyText(true)]
+        public string Password { get; set; } = null!;
+    }
+}
