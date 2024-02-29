895ccd7beb338c67a56ced127cbc4bdccd7205c2
Marina Sichova <msichova@outlook.com>
2024-02-29 11:32:05 +0800

    Create ApiRolesConstants.cs

---
+++ BooksStock.API/Services/Authentication/ApiRolesConstants.cs
@@ -0,0 +1,8 @@
+﻿namespace BooksStock.API.Services.Authentication
+{
+    public class ApiRolesConstants
+    {
+        public const string Admin = "admin";
+        public const string User = "user";
+    }
+}
